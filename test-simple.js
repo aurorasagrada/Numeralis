87692bde0cc1f63947b4c0beba1f33d03602d42d
aurorasagrada <210784449+aurorasagrada@users.noreply.github.com>
2025-10-28 14:15:28 -0400

    Adicionar teste simples para diagnosticar problema JavaScript

---
+++ test-simple.js
@@ -0,0 +1,13 @@
+console.log("JavaScript funcionando!");
+
+function preencherExemploMapa() {
+  document.getElementById("nomeCompleto").value = "Maria Silva Santos";
+  document.getElementById("dataNascimento").value = "1990-05-15";
+  console.log("Exemplo preenchido!");
+}
+
+function calcularMapaCompleto() {
+  alert("Funcao calcular funcionando!");
+}
+
+console.log("Arquivo test-simple.js carregado com sucesso!");
